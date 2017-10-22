14530ca0fe93bfe138201566c267905e0e47b62e
Motohiro Yakura <motohiro686@yahoo.co.jp>
2017-10-22 17:20:48 +0900

    echo

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function handleEvent(event) {
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: event.message.text + 'なんだシ〜〜' };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
